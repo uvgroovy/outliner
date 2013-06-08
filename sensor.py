744b47f90015cc150eacbc12ceead65e0a8c53d5
Yuval Kohavi <yuval.kohavi@gmail.com>
2013-06-08 13:31:52 -0400

    added hmtl ui

--- sensor.py
+++ sensor.py
@@ -1,41 +1,39 @@
 import RPi.GPIO as GPIO
 from time import time
 import struct
+from LSM303 import *
 
 PIN = 7
     
 CLICK_DISTANCE=6.25
 ADDRESS=0b0011110
 
-class Compass:
-    def __init__(self, address):
-        self.sock = None
-        
-    def getBearings(self):
-        s.send(struct.pack("!I", 1))
-        return struct.unpack("!I", s.recv(4))[0]
 
-client = None
+compass = LSM303()
 
-def getBearings():
-    return client.getBearings()
+def getHeadings():
+    return client.getHeadings()
 
 def handleCick():
-    bearing = getBearings()
+    bearing = getHeadings()
     print time(), CLICK_DISTANCE, bearing
-    
-def work():
-    GPIO.setmode(GPIO.BCM)
-    
-    GPIO.setup(PIN, GPIO.IN) 
-    GPIO.add_event_detect(PIN, GPIO.RISING, callback=handleCick)
 
+def init():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(PIN, GPIO.IN)
+        
+def getAngle():
+    GPIO.wait_for_edge(PIN, GPIO.FALLING)
+    return compass.getHeading()
+        
+def work():
+    while True:
+        print getAngle()
+    
     
 def main():
     try:
-        global client
-        address= ""
-        client = Compass(address)
+        init() 
         work()
     finally:
         GPIO.cleanup()
